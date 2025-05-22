ddf3131291ca000ac6fdf36421310e17ce26b2d4
Old2015 <old2015@gmail.com>
2025-05-22 18:53:37 +0200

    Fix Markdown escape to avoid escaping digits

--- utils.py
+++ utils.py
@@ -4,5 +4,6 @@ def escape_md(text: str) -> str:
     """Экранирует спецсимволы Markdown."""
     if not isinstance(text, str):
         return text
-    return re.sub(r'([*_\[\]()~`>#+-=|{}.!])', r'\\\1', text)
+    # place '-' at the end of the character class to avoid creating ranges
+    return re.sub(r'([*_\[\]()~`>#+=|{}.!-])', r'\\\1', text)
 
